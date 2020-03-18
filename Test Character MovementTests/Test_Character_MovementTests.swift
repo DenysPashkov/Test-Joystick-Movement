310f8bc8e80b3df11eb2a2f206d5b10379d55b3d
denys <dexixoxa@yahoo.com>
2020-03-18 14:55:42 +0100

    Initial Commit

---
+++ Test Character MovementTests/Test_Character_MovementTests.swift
@@ -0,0 +1,34 @@
+//
+//  Test_Character_MovementTests.swift
+//  Test Character MovementTests
+//
+//  Created by admin on 18/03/2020.
+//  Copyright © 2020 DenysPashkov. All rights reserved.
+//
+
+import XCTest
+@testable import Test_Character_Movement
+
+class Test_Character_MovementTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
